8969b6a137f435cad4a2e4c0e17710691ebdb5f7
Theo Madzou <madztheo@hotmail.fr>
2024-11-21 07:21:26 +0200

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,107 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import { ZkPassport, ProofResult, EU_COUNTRIES } from "zkpassport-sdk";
+import QRCode from "react-qr-code";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [message, setMessage] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [isEUCitizen, setIsEUCitizen] = useState<boolean | undefined>(
+    undefined
+  );
+  const [queryUrl, setQueryUrl] = useState("");
+  const [requestInProgress, setRequestInProgress] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+  let zkPassport: ZkPassport | null = null;
+  if (typeof window !== "undefined") {
+    zkPassport = new ZkPassport(window.location.hostname);
+  }
+
+  const createRequest = async () => {
+    if (!zkPassport) {
+      return;
+    }
+    setFirstName("");
+    setIsEUCitizen(undefined);
+    setMessage("");
+    setQueryUrl("");
+
+    const queryBuilder = await zkPassport.request({
+      name: "ZKPassport",
+      logo: "https://zkpassport.id/favicon.png",
+      purpose: "Proof of EU citizenship and firstname",
+    });
+
+    const {
+      url,
+      onQRCodeScanned,
+      onGeneratingProof,
+      onProofGenerated,
+      onReject,
+      onError,
+    } = queryBuilder
+      .in("nationality", EU_COUNTRIES)
+      .disclose("firstname")
+      .done();
+
+    setQueryUrl(url);
+    console.log(url);
+
+    setRequestInProgress(true);
+
+    onQRCodeScanned(() => {
+      console.log("QR code scanned");
+      setMessage("Request received");
+    });
+
+    onGeneratingProof(() => {
+      console.log("Generating proof");
+      setMessage("Generating proof...");
+    });
+
+    onProofGenerated(async (result: ProofResult) => {
+      console.log("Result of the query", result);
+      setFirstName(result?.queryResult.firstname?.disclose?.result);
+      setIsEUCitizen(result?.queryResult.nationality?.in?.result);
+      setMessage("Proof generated");
+      setRequestInProgress(false);
+    });
+
+    onReject(() => {
+      console.log("User rejected");
+      setMessage("User rejected the request");
+      setRequestInProgress(false);
+    });
+
+    onError((error: unknown) => {
+      console.error("Error", error);
+      setMessage("An error occurred");
+      setRequestInProgress(false);
+    });
+  };
+
+  return (
+    <main className="w-full h-full flex flex-col items-center p-10">
+      {queryUrl && <QRCode className="mb-4" value={queryUrl} />}
+      {message && <p>{message}</p>}
+      {firstName && (
+        <p className="mt-2">
+          <b>Firstname:</b> {firstName}
+        </p>
+      )}
+      {typeof isEUCitizen === "boolean" && (
+        <p className="mt-2">
+          <b>Is EU citizen:</b> {isEUCitizen ? "Yes" : "No"}
+        </p>
+      )}
+      {!requestInProgress && (
+        <button
+          className="p-4 mt-4 bg-gray-500 rounded-lg text-white font-medium"
+          onClick={createRequest}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          Generate new request
+        </button>
+      )}
+    </main>
   );
 }
